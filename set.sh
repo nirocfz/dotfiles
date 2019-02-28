83710ff942aeaaca72425174801ca204e6692da9
Chen Fangzhou <fangzhou@fydeos.io>
2019-02-28 13:56:48 +0800

    modified:   set.sh

--- set.sh
+++ set.sh
@@ -6,6 +6,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 bak_dir="$HOME/.dotfiles_bak/$(date +%s)"
 
 mkdir -p $bak_dir
+mkdir -p $HOME/.vim
 
 if [[ -e $HOME/.tmux.conf ]]; then
     mv $HOME/.tmux.conf $bak_dir
@@ -16,10 +17,16 @@ if [[ -e $HOME/.vimrc ]]; then
 fi
 
 if [[ -d $HOME/.vim/ftplugin ]]; then
-    mkdir $bak_dir/vim
+    mkdir -p $bak_dir/vim
     mv $HOME/.vim/ftplugin $bak_dir/vim
+fi
+
+if [[ -d $HOME/.vim/coc-settings.json ]]; then
+    mkdir -p $bak_dir/vim
+    mv $HOME/.vim/coc-settings.json $bak_dir/vim
 fi
 
 ln -s $script_dir/vim/vimrc $HOME/.vimrc
 ln -s $script_dir/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $script_dir/vim/ftplugin $HOME/.vim/ftplugin
+ln -s $script_dir/vim/coc-settings.json $HOME/.vim/coc-settings.json
